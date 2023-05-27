34c6411af911ace321507b595ad022c889dc15b6
Kaitlen Ngo <kaitlen100@gmail.com>
2023-05-27 14:11:33 -0700

    karaoke stuffs pls god dont break

---
+++ frontend/src/Karaoke.js
@@ -0,0 +1,20 @@
+import logo from "./logo.svg";
+import "./Karaoke.css";
+import { useState } from "react";
+
+function Karaoke(){
+    return(
+        <div className="App">
+            <header className="App-header">
+                <h1>
+                    Image goes here...
+                </h1>
+                <div>
+                    lyrics go here...
+                </div>
+            </header>
+        </div>
+    )
+}
+
+export default Karaoke;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,17 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Home from './Home'
-import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-    <Home />
+    <Apps/>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export default function Apps() {
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='karaoke' element={<Karaoke />} />
+        <Route path='app' element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
