e4e944e7bd24765f2243e9667e6c297a3c941b19
bdogetlauncher <bdogetlauncher@gmail.com>
2023-05-27 12:50:29 -0700

    Created get_lyrics.py

---
+++ backend/get_lyrics.py
@@ -0,0 +1,30 @@
+
+import requests
+import json
+
+def get_lyrics(track_id):
+    """
+        Returns an array of lyrics
+            [dict('startTimeMs': int, 'words': str)] // endtimeMs and syllables don't work
+        raises Exception if invalid track_id or no lyrics found or not LINE_SYNCED
+    """
+    link = f"https://spotify-lyric-api.herokuapp.com/?trackid={track_id}"
+    f = requests.get(link)
+    
+    if f.status_code != 200:
+        raise Exception("Error: get_lyrics.py: get_lyrics: status code not 200")
+    data = json.loads(f.text)
+    if data["error"] == True:
+        raise Exception("Error: get_lyrics.py: No Lyrics Found")
+    if data["syncType"] != "LINE_SYNCED":
+        raise Exception("Error: get_lyrics.py: Lyrics are not LINE_SYNCED")
+    
+    return data['lines']
+
+
+if __name__ == "__main__":
+    track_id = "11dFghVXANMlKmJXsNCbNl" # "5f8eCNwTlr0RJopE9vQ6mB"
+    lyrics = get_lyrics(track_id)
+
+    print(lyrics)
+    print(type(lyrics))
